988a027864166294662874d7ded9a24f6ab848d5
vgugnani <vanshajgugnani04@gmail.com>
2025-01-09 15:02:04 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set up static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+
+// Basic route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
